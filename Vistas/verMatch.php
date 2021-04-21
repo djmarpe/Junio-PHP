c0ed1d1ed08c42eef9e20502201b0d7a80ba5893
Alejandro <alejandro.martin.perez.99@gmail.com>
2021-04-21 18:34:27 +0200

    Ver a la persona en detalle completado

--- Vistas/verMatch.php
+++ Vistas/verMatch.php
@@ -7,11 +7,144 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/all.min.css">
+        <link rel="stylesheet" type="text/css" href="../css/miCss.css">
         <title>Match!</title>
     </head>
-    <body>
+    <body class="background-dark-blue">
         <?php
-        // put your code here
+        require_once '../Clases/Persona.php';
+        require_once '../Clases/UsuarioPreferencias.php';
+        session_start();
+        $usuarioLogin = $_SESSION['usuarioLogin'];
+        $usuarioMatch = $_SESSION['usuarioElegido'];
         ?>
-    </body>
+        <div class="row m-3">
+            <div class="col-12 bg-white align-self-center py-2">
+                <div class="row m-0">
+                    <div class="col-6 bg-white align-self-center">
+                        <small class="text-muted float-start align-self-center">Conectado como: <?= $usuarioLogin->getName() . ' ' . $usuarioLogin->getSurname() ?></small>
+                    </div>
+                    <div class="col-6 bg-white">
+                        <a href="../controladores/controlador.php?cerrarSesion=cerrarSesion" class="btn btn-primary float-end">Cerrar sesión</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row m-3">
+            <div class="col-12 bg-white">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb my-2">
+                        <?php
+                        if ($usuarioLogin->getRol() == 'Administrador') {
+                            ?>
+                            <li class = "breadcrumb-item" aria-current = "page"><a href = "../controladores/controlador.php?dashboardAdmin=dashboardAdmin" class = "text-decoration-underline text-dark">Dashboard</a></li>
+                            <?php
+                        } else if ($usuarioLogin->getRol() == 'Usuario') {
+                            ?>
+                            <li class = "breadcrumb-item" aria-current = "page"><a href = "../controladores/controlador.php?dashboardUsuario=dashboardUsuario" class = "text-decoration-underline text-dark">Dashboard</a></li>
+                            <?php
+                        }
+                        ?>
+                        <li class = "breadcrumb-item" aria-current = "page"><a href = "../controladores/controlador.php?verGenteCercana=verGenteCercana" class = "text-decoration-underline text-dark">Gente cercana</a></li>
+                        <li class="breadcrumb-item active" aria-current="page"><?= $usuarioMatch->getNombreUsuario() . ' ' . $usuarioMatch->getApellidosUsuario() ?></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row m-3">
+            <div class="col-12 bg-white">
+                <input type="hidden" name="nombre" value="<?= $usuarioMatch->getNombreUsuario() ?>">
+                <input type="hidden" name="apellido" value="<?= $usuarioMatch->getApellidosUsuario() ?>">
+                <h3 class="my-5 text-decoration-underline text-center"><?= $usuarioMatch->getNombreUsuario() . ' ' . $usuarioMatch->getApellidosUsuario() ?></h3>
+                <div class="row m-0">
+                    <div class="col-12 col-sm-6">
+                        <input type="hidden" id="idUsuario" value="<?= $usuarioMatch->getIdUsuario() ?>">
+                        <p><?= $usuarioMatch->getDescripcion() ?></p>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <div class="row m-0">
+                            <div class="col-12 col-sm-6 my-3">
+                                <strong>País:</strong>
+                                <input type="hidden" id="pais" value="<?= $usuarioMatch->getPais() ?>">
+                                <small><?= $usuarioMatch->getPais() ?></small>
+                            </div>
+                            <div class="col-12 col-sm-6 my-3">
+                                <strong>Localidad:</strong>
+                                <input type="hidden" id="localidad" value="<?= $usuarioMatch->getLocalidad() ?>">
+                                <small><?= $usuarioMatch->getLocalidad() ?></small>
+                            </div>
+                            <div class="col-12 col-sm-6 my-3">
+                                <strong>Fecha de nacimiento:</strong>
+                                <input type="hidden" id="fechaNac" value="<?= $usuarioMatch->getFechaNacimientoUsuario() ?>">
+                                <small><?= $usuarioMatch->getFechaNacimientoUsuario() ?></small>
+                            </div>
+                            <div class="col-12 col-sm-6 my-3">
+                                <strong>Correo electrónico:</strong>
+                                <input type="hidden" id="emailMatch" value="<?= $usuarioMatch->getEmail() ?>">
+                                <input type="hidden" id="emailUsuario" value="<?= $usuarioLogin->getEmail() ?>">
+                                <small><?= $usuarioMatch->getEmail() ?></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row m-0">
+                    <form action="../controladores/controlador.php" method="POST" class="col-12 my-3 text-center">
+                        <input type="submit" name="conectarAmigo" value="Conectar" class="btn btn-primary">
+                        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                            Enviar mensaje
+                        </button>
+                        <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Enviar un mensaje</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="../controladores/controlador.php">
+                                            <div class="mb-2">
+                                                <small class="d-block float-start">De:</small>
+                                                <input type="email" name="email_from" class="form-control" id="emailUsuarioEmisor" readonly>
+                                            </div>
+
+                                            <div class="my-2">
+                                                <small class="d-block float-start">Para:</small>
+                                                <input type="email" name="email_to" class="form-control" id="emailUsuarioReceptor" readonly>
+                                            </div>
+                                            
+                                            <div class="my-2">
+                                                <small class="d-block float-start">Asunto:</small>
+                                                <input type="email" name="subject" class="form-control">
+                                            </div>
+
+                                            <div class="my-2">
+                                                <small class="d-block float-start">Mensaje:</small>
+                                                <textarea type="text" rows="5" name="text" class="form-control"></textarea>
+                                            </div>
+                                            
+                                            <input type="submit" class="btn btn-primary my-2" value="Enviar" name="enviarMensaje">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <script type="text/javascript">
+                            var emailMatch = document.getElementById('emailMatch').value
+                            var emailLogin = document.getElementById('emailUsuario').value
+                            document.getElementById('emailUsuarioEmisor').value = emailLogin;
+                            document.getElementById('emailUsuarioReceptor').value = emailMatch;
+                        </script>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="../js/all.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</body>
 </html>
--- js/miJS.js
+++ js/miJS.js
@@ -17,3 +17,5 @@ rangePolitica = ()  => {
 }
 
 
+
+
